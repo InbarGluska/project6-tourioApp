f875433c0bec7362958d3dc1d081bd8a0285b854
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-07-14 10:48:43 +0200

    feat: task 2

--- recap-project-6_tourio-app/pages/api/places/index.js
+++ recap-project-6_tourio-app/pages/api/places/index.js
@@ -1,4 +1,5 @@
 // import { places } from "../../../lib/db";
+
 import dbConnect from "./db/connect.js";
 import Place from "./db/models/Place";
 
@@ -8,6 +9,17 @@ export default async function handler(request, response) {
     const places = await Place.find();
 
     return response.status(200).json(places);
+  }
+  if (request.method === "POST") {
+    try {
+      const placeData = request.body;
+      const place = new Place(placeData);
+      await place.save();
+      response.status(201).json({ status: "Place created" });
+    } catch (error) {
+      console.log(error);
+      response.status(400).json({ error: error.message });
+    }
   } else {
     return response.status(405).json({ message: "Method not allowed" });
   }
